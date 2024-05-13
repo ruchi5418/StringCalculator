b5d2913462cebddb2ae6f94ee1c281766cb12e84
ruchiKadam <ruchi.kadam@aretove.com>
2024-05-13 20:35:19 +0530

    Write function to calculate the sum of numbers from strings

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,10 +1,33 @@
 require 'rspec/autorun'
 
 def StringCalculator(numbers)
-  return 0 if numbers.empty?
-  numbers.to_i if numbers.size == 1
+    return 0 if numbers.empty?
+    numbers.to_i if numbers.size == 1
 
-  numbers.split(",").map(&:to_i).sum
+    numbers.split(",").map(&:to_i).sum
+
+    delimiter = ","
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers[(numbers.index("\n") + 1)..-1]
+    end
+
+    numbers = numbers.split(/#{delimiter}|\n/)
+
+    sum = 0
+    negatives = []
+
+    numbers.each do |num|
+      if num.to_i.negative?
+        negatives << num
+      else
+        sum += num.to_i
+      end
+    end
+
+    raise "Negative numbers not allowed: #{negatives.join(',')}" unless negatives.empty?
+
+    sum
 end
 
 describe "#StringCalculator" do 
@@ -23,6 +46,30 @@ describe "#StringCalculator" do
     context "with two numbers" do
         it "returns the sum of the numbers" do
             expect(StringCalculator("1,2")).to eq(3)
+        end
+    end
+
+    context "with newlines between numbers" do
+        it "returns the sum of the numbers" do
+            expect(StringCalculator("1\n2,3")).to eq(3)
+        end
+      end
+  
+      context "with custom delimiter" do
+        it "returns the sum of the numbers" do
+            expect(StringCalculator("//;\n1;2")).to eq(3)
+        end
+      end
+  
+      context "with negative numbers" do
+        it "raises an exception" do
+            expect(StringCalculator("-1,2")).to raise_error("Negative numbers not allowed: -1")
+        end
+      end
+  
+      context "with multiple negative numbers" do
+        it "raises an exception" do
+          expect { StringCalculator("-1,-2,3,-4") }.to raise_error("Negative numbers not allowed: -1,-2,-4")
         end
       end
 end
