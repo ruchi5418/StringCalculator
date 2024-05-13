ecf31a987c19aea3cb5c6d3e705564cc3765f816
ruchiKadam <ruchi.kadam@aretove.com>
2024-05-13 20:28:23 +0530

    Add test and functionality for two numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -2,7 +2,9 @@ require 'rspec/autorun'
 
 def StringCalculator(numbers)
   return 0 if numbers.empty?
-  numbers.to_i # single number
+  numbers.to_i if numbers.size == 1
+
+  numbers.split(",").map(&:to_i).sum
 end
 
 describe "#StringCalculator" do 
@@ -17,4 +19,10 @@ describe "#StringCalculator" do
             expect(StringCalculator("1")).to eq(1)
         end
     end
+
+    context "with two numbers" do
+        it "returns the sum of the numbers" do
+            expect(StringCalculator("1,2")).to eq(3)
+        end
+      end
 end
